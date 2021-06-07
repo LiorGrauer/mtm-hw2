4917647afea0c631e5dd0947ecb9254a972bad27
LiorGrauer <84275385+LiorGrauer@users.noreply.github.com>
2021-06-07 16:02:12 +0300

    Update ExamDetails.cpp

--- ExamDetails.cpp
+++ ExamDetails.cpp
@@ -1,5 +1,13 @@
+#include <fstream>
+using std::string;
+
 namespace mtm {
     class ExamDetails {
-
+        int course_number, exam_month, exam_day, length;
+        double exam_hour; 
+        string zoom_link;
+        
+        public:
+            das
     };
 }
