a11d57ff74ccd42240874de3b9790d56f88f0435
Tom R <tomrahav500@gmail.com>
2021-06-12 11:17:36 +0300

    added h and cpp of charcters

--- partB/Character.h
+++ partB/Character.h
@@ -1,20 +1,44 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
-class Character{
-    private:
+#include "./Auxiliaries.h"
+#include "./Exceptions.h"
 
-    public:
-        virtual checkMove;
-        virtual load;
-        virtual checkAttack;
-        virtual checkIncidentalDamageRange;
-        virtual attack;
+namespace mtm{
+    class Character{
+        private:
+            Team team;
+            units_t range, power;
+        
+        protected:
+            units_t health, ammo;
 
-    protected:
-        int health, ammo, range, power;
-
-
-};
-
+        public:
+            Character(Team team, units_t health, units_t ammo, units_t range,units_t power) :
+                team(team), health(health), ammo(ammo), range(range), power(power) {}
+            virtual ~Character() {};
+            units_t getRange() const{
+                return range;
+            };
+            units_t getPower() const{
+                return power;
+            };
+            units_t getHealth() const{
+                return health;
+            };
+            Team getTeam() const{
+                return team;
+            };
+            void changeHealth(int change){
+                health+=change;
+            };
+            virtual bool checkMove(GridPoint src_point, GridPoint dst_point) = 0;
+            virtual void load() = 0;
+            virtual bool checkAttack(GridPoint src_point, GridPoint dst_point) = 0;
+            virtual units_t getIncidentalDamageRange(){
+                return 0;
+            };
+            virtual units_t attack() = 0;
+    };
+}
 #endif
