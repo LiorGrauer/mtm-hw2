d08f629469fcbf59de2598cbe5595d2f1f7fca3b
Tom R <tomrahav500@gmail.com>
2021-06-12 09:52:49 +0300

    update char

--- Character.h
+++ Character.h
@@ -1,5 +1,20 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
+class Character{
+    private:
+
+    public:
+        virtual checkMove;
+        virtual load;
+        virtual checkAttack;
+        virtual checkIncidentalDamageRange;
+        virtual attack;
+
+    protected:
+        int health, ammo, range, power;
+
+
+};
 
 #endif
