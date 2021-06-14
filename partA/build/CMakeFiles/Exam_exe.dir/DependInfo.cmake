14b0d468b259e1e0e5cc94066ab8624efb729e29
LiorGrauer <84275385+LiorGrauer@users.noreply.github.com>
2021-06-14 12:59:15 +0300

    Merge branch 'main' of https://github.com/LiorGrauer/mtm-hw2

---
+++ partA/build/CMakeFiles/Exam_exe.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/liorg/OneDrive - Technion/Documents/GitHub/mtm-hw2/partA/examDetails.cpp" "C:/Users/liorg/OneDrive - Technion/Documents/GitHub/mtm-hw2/partA/build/CMakeFiles/Exam_exe.dir/examDetails.cpp.obj"
+  "C:/Users/liorg/OneDrive - Technion/Documents/GitHub/mtm-hw2/partA/part_a_test.cpp" "C:/Users/liorg/OneDrive - Technion/Documents/GitHub/mtm-hw2/partA/build/CMakeFiles/Exam_exe.dir/part_a_test.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- partA/build/CMakeFiles/Makefile.cmake
+++ partA/build/CMakeFiles/Makefile.cmake
@@ -45,5 +45,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/GAME_exe.dir/DependInfo.cmake"
+  "CMakeFiles/Exam_exe.dir/DependInfo.cmake"
   )
