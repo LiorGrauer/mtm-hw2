23b3ecb709c88df89e8900a666cab47d2fc2f455
LiorGrauer <84275385+LiorGrauer@users.noreply.github.com>
2021-06-16 19:12:20 +0300

    conventions

--- partB/Soldier.cpp
+++ partB/Soldier.cpp
@@ -3,7 +3,7 @@
 namespace mtm{
 
     bool Soldier::checkMove(GridPoint src_point, GridPoint dst_point) {
-        if(GridPoint::distance(src_point,dst_point)<=SOLDIER_MOVE_DISTANCE){
+        if(GridPoint::distance(src_point,dst_point) <= SOLDIER_MOVE_DISTANCE){
             return true;
         } else {
             return false;
@@ -11,7 +11,7 @@ namespace mtm{
     }
 
     void Soldier::load(){
-        ammo+=SOLDIER_AMMO_LOAD;
+        ammo += SOLDIER_AMMO_LOAD;
     }
 
     bool Soldier::checkAttack(GridPoint src_point, GridPoint dst_point,
@@ -19,14 +19,14 @@ namespace mtm{
         if(GridPoint::distance(src_point,dst_point)>getRange()){
             throw OutOfRange();
         }
-        if(ammo<SOLDIER_AMMO_ATTACK)
+        if(ammo < SOLDIER_AMMO_ATTACK)
         {
             throw OutOfAmmo();
         }
         if(src_point.col != dst_point.col && src_point.row != dst_point.row){
             throw IllegalTarget();
         }
-        ammo-=SOLDIER_AMMO_ATTACK;
+        ammo -= SOLDIER_AMMO_ATTACK;
         return true;
     }
 
@@ -34,7 +34,7 @@ namespace mtm{
                                     Team damage_point_team){
         double double_power = getPower();
         if(damage_point_team != getTeam()){
-            if(dst_point==damage_point){
+            if(dst_point == damage_point){
                 return getPower();
             } else {
                 return ceil(double_power/SOLDIER_INCIDENTAL_DAMAGE_DIVIDER);
