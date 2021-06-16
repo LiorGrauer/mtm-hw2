d16844d6bbe2c92725a21608375a5e3b724c6656
Tom R <tomrahav500@gmail.com>
2021-06-16 16:45:01 +0300

    Revert "Merge branch 'main' of https://github.com/LiorGrauer/mtm-hw2 into main"
    This reverts commit 3c20f28686cd3eee22d38a3d03b3e8557f74e38c, reversing
    changes made to d25248927d6947cd62248229d5cc6b1a1107a7c0.

--- partB/Exceptions.h
+++ partB/Exceptions.h
@@ -5,8 +5,6 @@
 
 namespace mtm {
     class Exception: public std::exception {
-        public:
-            const char* what() const noexcept override = 0;
     };
 
     class IllegalArgument: public Exception {
