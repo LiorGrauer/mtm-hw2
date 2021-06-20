ec3c286cc68e9c4a6f243a2be7693b5e43351506
LiorGrauer <84275385+LiorGrauer@users.noreply.github.com>
2021-06-20 22:24:24 +0300

    character.cpp fixed

--- partB/Character.cpp
+++ partB/Character.cpp
@@ -3,30 +3,30 @@
 namespace mtm{
 
     Character::Character(Team team, units_t health, units_t ammo, units_t range ,units_t power) :
-                team(team), range(range), power(power), health(health), ammo(ammo) {};
+                team(team), range(range), power(power), health(health), ammo(ammo) {}
 
     units_t Character::getRange() const{
         return range;
-    };
+    }
 
     units_t Character::getPower() const{
         return power;
-    };
+    }
 
     units_t Character::getHealth() const{
         return health;
-    };
+    }
 
     Team Character::getTeam() const{
         return team;
-    };
+    }
 
     void Character::changeHealth(int change){
         health -= change;
-    };
+    }
 
     units_t Character::getIncidentalDamageRange(){
         return 0;
-    };
+    }
     
 }
