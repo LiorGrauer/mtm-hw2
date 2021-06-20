31d0f08b5d6f5689491e9895f02443d9844c8057
LiorGrauer <84275385+LiorGrauer@users.noreply.github.com>
2021-06-20 15:26:48 +0300

    doc added to exam details

--- partA/examDetails.h
+++ partA/examDetails.h
@@ -12,17 +12,102 @@ namespace mtm {
             std::string link;
             
         public:
+            /**
+             * ExamDetails: Exam Details constructor
+             *
+             * @param course_number - course number. Must be type int
+             * @param course_number - month. Must be type int
+             * @param month - month. Must be type int
+             * @param day - day. Must be type int
+             * @param hour - hour. Must be type double, only .0 or .5
+             * @param length - length. Must be type int
+             * @param link - link. Must be type string
+             *
+             * @return
+             *     Exam Details with given data 
+             */
             ExamDetails(int course_number, int month, int day, double hour, int length, std::string link = "");
-            ~ExamDetails() = default;
-            ExamDetails(const ExamDetails& exam);
-            ExamDetails& operator=(const ExamDetails& exam);
-            std::string getLink() const;
-            void setLink(std::string link);
-            int operator-(const ExamDetails& exam) const;
-            bool operator<(const ExamDetails& exam) const;
-            friend std::ostream& operator<<(std::ostream& os, const ExamDetails& exam);
-            static ExamDetails makeMatamExam();
 
+            // default destructor
+            ~ExamDetails() = default;
+
+            /**
+             * ExamDetails: Exam Details copy constructor
+             *
+             * @param s - ExamDetails&. existing Exam Details.
+             *
+             * @return
+             *     new copy of the given sorted list
+             */
+            ExamDetails(const ExamDetails& exam);
+
+            /**
+             * operator=: Exam Details placement operator
+             *
+             * @param exam - ExamDetails&. existing Exam Details
+             *
+             * @return
+             *     inserts the data from the given Exam Details to an existing Exam Details
+             *     deletes all the old data and inserts the new from the given Exam Details.
+             */
+            ExamDetails& operator=(const ExamDetails& exam);
+
+            /**
+             * getLink: returns Exam Details zoom link
+             *
+             * @return 
+             *      zoom link
+             *
+             */
+            std::string getLink() const;
+
+            /**
+             * setLink: changes Exam Details zoom link
+             *
+             * @param link - new zoom link
+             * 
+             */
+            void setLink(std::string link);
+
+            /**
+             * operator-: Exam Details subtraction operator
+             *
+             * @param exam - ExamDetails&. Exam Details
+             *
+             * @return
+             *     return the differnce in days between the two exams
+             */
+            int operator-(const ExamDetails& exam) const;
+
+            /**
+             * operator<: Exam Details smaller than operator
+             *
+             * @param exam - ExamDetails&. Exam Details
+             *
+             * @return
+             *     true/false - return true if the first exam is before the second.
+             */
+            bool operator<(const ExamDetails& exam) const;
+
+            /**
+             * operator<<: Exam Details printing operator
+             *
+             * @param os - stream to add to. must be ostream type
+             *
+             * @return
+             *     stream after adding the exam details
+             */
+            friend std::ostream& operator<<(std::ostream& os, const ExamDetails& exam);
+
+            /**
+             * makeMatamExam: makes matam Exam Details
+             *
+             * @return
+             *     ExamDetails with the matam exam 
+             */
+            static ExamDetails makeMatamExam();
+            
+            // exceptions for ExamDetails
             class InvalidDateException {};
             class InvalidTimeException {};
             class InvalidArgsException {};
--- partA/sortedList.h
+++ partA/sortedList.h
@@ -13,49 +13,55 @@ namespace mtm{
         Node<T>* next_node;
 
         public:
+
+        /**
+         * Node: sorted list node constructor
+         *
+         * @param data - data. Must be type T
+         *
+         * @return
+         *     node with given data and null pointer
+         */
         explicit Node(const T& data);
+
+        //default destructer
         ~Node() = default;
+
+        /**
+         * getData: returns data in node
+         *
+         * @return
+         *     const T type data
+         */
         const T& getData() const;
+
+        /**
+         * getNext: returns next node pointer in the node
+         *
+         * @return
+         *     Node<T> pointer to the next node in the list
+         */
         Node* getNext() const;
+
+        /** setNext: sets next node pointer in the node */
         void setNext(Node* ptr);
     };
 
-    /**
-     * Node: sorted list node constructor
-     *
-     * @param data - data. Must be type T
-     *
-     * @return
-     *     node with given data and null pointer
-     */
     template <class T>
     Node<T>::Node(const T& data) :
         data(data), next_node(nullptr) {
     }
 
-    /**
-     * getData: returns data in node
-     *
-     * @return
-     *     T type data
-     */
     template <class T>
     const T& Node<T>::getData() const{
         return data;
     }
     
-    /**
-     * getNext: returns next node pointer in the node
-     *
-     * @return
-     *     Node<T> pointer to the next node in the list
-     */
     template <class T>
     Node<T>* Node<T>::getNext() const{
         return next_node;
     }
     
-    /** setNext: sets next node pointer in the node */
     template <class T>
     void Node<T>::setNext(Node* ptr){
         next_node = ptr;
@@ -66,18 +72,100 @@ namespace mtm{
     class SortedList {
         public:
             class const_iterator;
+            
+            /**
+             * SortedList: sorted list constructor
+             *
+             * @return
+             *     empty sorted list
+             */
             explicit SortedList();
+
+            /** ~SortedList: sorted list deconstructor. erases all the data nodes in the sorted list */
             ~SortedList();
+
+            /**
+             * SortedList: sorted list node copy constructor
+             *
+             * @param s - SortedList<T>&. existing sorted list from data type T
+             *
+             * @return
+             *     new copy of the given sorted list
+             */
             SortedList(const SortedList<T>& s);
+
+            /**
+             * operator=: sorted list placement operator
+             *
+             * @param s - SortedList<T>&. existing sorted list from data type T
+             *
+             * @return
+             *     inserts the data from the given sorted list to an existing sorted list
+             *     deletes all the old data and inserts the new from the given sorted list.
+             */
             SortedList<T>& operator=(const SortedList<T>&);
+
+            /**
+             * insert: inserts given data type T to the sorted list
+             *
+             * @param t - T&. reference to data type T
+             *
+             */
             void insert(const T& t);
+
+            /**
+             * remove: remove specific data from the sorted list by given iterator
+             *
+             * @param t - const_iterator& . reference to iterator
+             *
+             */
             void remove(const const_iterator& iter);
+
+            /**
+             * length: returns sorted list length
+             *
+             * @return 
+             *      length of the sorted list
+             *
+             */
             int length() const;
+
+            /**
+             * filter: creates new sorted list that contains only objects that meet filter condition
+             *
+             * @param c - Condition. bool function that recives data type T and returns if met condition or not
+             *
+             * @return
+             *     new sorted list that containes only objects that met the condition.
+             */
             template<class Condition>
             SortedList<T> filter(Condition c) const;
+
+             /**
+             * apply: creates new sorted list that contains all objects after apply function was activated on them
+             *
+             * @param f - Function. function that recives data type T and returns it after appling something on it
+             *
+             * @return
+             *     new sorted list that containes all objects after apply function was applied on them.
+             */
             template<class Function>
             SortedList<T> apply(Function f) const;
+
+            /**
+             * begin: return iterator that points to the first node in the sorted list
+             *
+             * @return
+             *     iterator the point to the first node
+             */
             const_iterator begin() const;
+
+            /**
+             * end: return iterator that points to the next node attribute of the last node in the sorted list (nullptr)
+             *
+             * @return
+             *     iterator the point to nullptr
+             */
             const_iterator end() const;
 
         private:
@@ -86,12 +174,6 @@ namespace mtm{
             const_iterator iterator;
     };
 
-    /**
-     * SortedList: sorted list constructor
-     *
-     * @return
-     *     empty sorted list
-     */
     template <class T>
     SortedList<T>::SortedList() :
         head_node(nullptr), 
@@ -99,7 +181,6 @@ namespace mtm{
         iterator(head_node) {
     }
 
-    /** ~SortedList: sorted list deconstructor. erases all the data nodes in the sorted list */
     template <class T>
     SortedList<T>::~SortedList(){
         Node<T>* ptr = head_node;
@@ -110,14 +191,6 @@ namespace mtm{
         }
     }
 
-    /**
-     * SortedList: sorted list node copy constructor
-     *
-     * @param s - SortedList<T>&. existing sorted list from data type T
-     *
-     * @return
-     *     new copy of the given sorted list
-     */
     template <class T>
     SortedList<T>::SortedList(const SortedList<T>& s) :
         head_node(nullptr),
@@ -130,15 +203,6 @@ namespace mtm{
         }
     }
 
-    /**
-     * operator=: sorted list placement operator
-     *
-     * @param s - SortedList<T>&. existing sorted list from data type T
-     *
-     * @return
-     *     inserts the data from the given sorted list to an existing sorted list
-     *     deletes all the old data and inserts the new from the given sorted list.
-     */
     template <class T>
     SortedList<T>& SortedList<T>::operator=(const SortedList<T>& s) {
         if (this == &s) {
@@ -161,12 +225,6 @@ namespace mtm{
         return *this;
     }
 
-    /**
-     * insert: inserts given data type T to the sorted list
-     *
-     * @param t - T&. reference to data type T
-     *
-     */
     template <class T>
     void SortedList<T>::insert(const T& t) {
         Node<T>* new_node_ptr = new Node<T>(t);
@@ -191,12 +249,6 @@ namespace mtm{
         size++;
     }
 
-    /**
-     * remove: remove specific data from the sorted list by given iterator
-     *
-     * @param t - const_iterator& . reference to iterator
-     *
-     */
     template <class T>
     void SortedList<T>::remove(const const_iterator& iter){
         SortedList<T>::const_iterator it(this->begin());
@@ -223,26 +275,11 @@ namespace mtm{
         }
     }
 
-    /**
-     * length: returns sorted list length
-     *
-     * @return 
-     *      length of the sorted list
-     *
-     */
     template <class T>
     int SortedList<T>::length() const {
         return size;
     }
 
-    /**
-     * filter: creates new sorted list that contains only objects that meet filter condition
-     *
-     * @param c - Condition. bool function that recives data type T and returns if met condition or not
-     *
-     * @return
-     *     new sorted list that containes only objects that met the condition.
-     */
     template<class T>
     template<class Condition>
     SortedList<T> SortedList<T>::filter(Condition c) const {
@@ -255,14 +292,6 @@ namespace mtm{
         return result;
     }
     
-    /**
-     * apply: creates new sorted list that contains all objects after apply function was activated on them
-     *
-     * @param f - Function. function that recives data type T and returns it after appling something on it
-     *
-     * @return
-     *     new sorted list that containes all objects after apply function was applied on them.
-     */
     template<class T>
     template<class Function>
     SortedList<T> SortedList<T>::apply(Function f) const {
@@ -273,23 +302,11 @@ namespace mtm{
         return result;
     }
 
-    /**
-     * begin: return iterator that points to the first node in the sorted list
-     *
-     * @return
-     *     iterator the point to the first node
-     */
     template <class T>
     typename SortedList<T>::const_iterator SortedList<T>::begin() const {
         return const_iterator(head_node);
     }
 
-    /**
-     * end: return iterator that points to the next node attribute of the last node in the sorted list (nullptr)
-     *
-     * @return
-     *     iterator the point to nullptr
-     */
     template <class T>
     typename SortedList<T>::const_iterator SortedList<T>::end() const {
         return const_iterator(nullptr);
@@ -300,53 +317,86 @@ namespace mtm{
     class SortedList<T>::const_iterator {
         private:
             const Node<T>* ptr;
+
+            /**
+             * const_iterator: const_iterator constructor
+             *
+             * @param node_ptr - const Node<T>*. pointer to Node
+             * 
+             * @return
+             *     iterator that points to the given pointer
+             */
             explicit const_iterator(const Node<T>* node_ptr);
+
             friend class SortedList<T>;
 
         public:
+            /**
+             * const_iterator: const_iterator copy constructor
+             *
+             * @param iter - const const_iterator&. existing iterator
+             * 
+             * @return
+             *     a copy of the given iterator
+             */
             const_iterator(const const_iterator& iter);
+
+            // default destructor
             ~const_iterator() = default;
+
+            /**
+             * const_iterator: const_iterator placement constructor
+             *
+             * @param node_ptr - const const_iterator&. existing iterator
+             * 
+             * @return
+             *     inserts the pointer of the given iterator to the existing iterator.
+             */
             const_iterator& operator=(const const_iterator& iter);
+
+            /**
+             * operator++: advanced the iterator and returns it after advancment
+             * 
+             * @return
+             *     Advanced iterator
+             */
             const_iterator& operator++();
+
+            /**
+             * operator++: advanced the iterator and returns it before advancment
+             * 
+             * @return
+             *     Pre advancedment iterator
+             */
             const_iterator operator++(int);
+
+            /**
+             * operator==: check if two iterators are equel
+             * 
+             * @return
+             *     true if both are the same, false if not
+             */
             bool operator==(const const_iterator& iter) const;
+
+            /**
+             * operator*: returns the data in the node the iterator has its pointer
+             * 
+             * @return
+             *     data type T
+             */
             const T& operator*() const;
     };
 
-    /**
-     * const_iterator: const_iterator constructor
-     *
-     * @param node_ptr - const Node<T>*. pointer to Node
-     * 
-     * @return
-     *     iterator that points to the given pointer
-     */
     template <class T>
     SortedList<T>::const_iterator::const_iterator(const Node<T>* node_ptr) :
         ptr(node_ptr) {
     }
 
-    /**
-     * const_iterator: const_iterator copy constructor
-     *
-     * @param iter - const const_iterator&. existing iterator
-     * 
-     * @return
-     *     a copy of the given iterator
-     */
     template <class T>
     SortedList<T>::const_iterator::const_iterator(const const_iterator& iter) : 
         ptr(iter.ptr) {
     }
 
-    /**
-     * const_iterator: const_iterator placement constructor
-     *
-     * @param node_ptr - const const_iterator&. existing iterator
-     * 
-     * @return
-     *     inserts the pointer of the given iterator to the existing iterator.
-     */
     template <class T>
     typename SortedList<T>::const_iterator& SortedList<T>::const_iterator::operator=(const const_iterator& iter){
         if (this == &iter) {
@@ -356,12 +406,6 @@ namespace mtm{
         return *this;
     }
 
-    /**
-     * operator++: advanced the iterator and returns it after advancment
-     * 
-     * @return
-     *     Advanced iterator
-     */
     template <class T>
     typename SortedList<T>::const_iterator& SortedList<T>::const_iterator::operator++(){
         if (!ptr){
@@ -371,12 +415,6 @@ namespace mtm{
         return *this;
     }
 
-    /**
-     * operator++: advanced the iterator and returns it before advancment
-     * 
-     * @return
-     *     Pre advancedment iterator
-     */
     template <class T>
     typename SortedList<T>::const_iterator SortedList<T>::const_iterator::operator++(int){
         if (!ptr){
@@ -387,28 +425,15 @@ namespace mtm{
         return result;
     }
 
-    /**
-     * operator==: check if two iterators are equel
-     * 
-     * @return
-     *     true if both are the same, false if not
-     */
     template <class T>  
     bool SortedList<T>::const_iterator::operator==(const const_iterator& iter) const{
         return ptr == iter.ptr;
     }
 
-    /**
-     * operator*: returns the data in the node the iterator has its pointer
-     * 
-     * @return
-     *     data type T
-     */
     template <class T>
     const T& SortedList<T>::const_iterator::operator*() const{
         return ptr->getData();
     }
-
 }
 
 #endif
